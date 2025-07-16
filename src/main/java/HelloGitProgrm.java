2d9eaf5dff82511e09bb36982f307a8d06a5ab6c
durmallika <durmallika.thapa@gmail.com>
2025-07-16 17:58:07 -0500

    this is the second commit.

---
+++ src/main/java/HelloGitProgrm.java
@@ -0,0 +1,13 @@
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+
+
+public class HelloGitProgrm {
+  public static void main(String[] args) {
+    System.out.println( " this is test to the 2nd commit");
+
+    System.out.println(" this is hello git program.");
+  }
+
+
+
+}
